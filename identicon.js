0c3700bbf7b31eb007f7383f0a8c24af4f62a9e3
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-09-25 10:12:35 +0200

    Size updated 	modified:   identicon.js

--- identicon.js
+++ identicon.js
@@ -7,7 +7,7 @@ const jdenticon = require("jdenticon");
 const fs = require("fs");
 
 const value = myArgs[0];
-const size = 200;
+const size = 600;
 
 const png = jdenticon.toPng(value, size);
 fs.writeFileSync(myArgs[1], png);
